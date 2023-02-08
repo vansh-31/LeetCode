d5d520baf271afc14802f78f1f8a9d14a4dc32ce
Vansh <vanshgawra008@gmail.com>
2023-02-08 15:50:06 +0530

    Solved today's challange using DP

---
+++ Day39.cpp
@@ -0,0 +1,35 @@
+// Problem : Jump Game II
+// Problem Statement : You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+// Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+// 0 <= j <= nums[i] and
+// i + j < n
+// Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define MOD 1000000007
+#define PI 3.14
+using namespace std;
+class Solution
+{
+public:
+    int solve(vector<int> &nums,int n,int index)
+    {
+        if (n==0)
+        {
+            return 1;
+        }
+        
+    }
+    int jump(vector<int> &nums)
+    {
+        
+    }
+};
+int main()
+{
+
+    return 0;
+}

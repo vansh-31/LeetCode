c26d95263beb8a04c9904425217a6e0593b3f700
Vansh <vanshgawra008@gmail.com>
2023-03-29 09:35:04 +0530

    Optimized the Space

--- Day88.py
+++ Day88.py
@@ -7,10 +7,12 @@ class Solution:
     def maxSatisfaction(self, sat: list[int]) -> int:
         n = len(sat)
         sat.sort()
-        dp = [[0 for x in range(n+2)] for y in range(n+2)]
+        curr = [0 for x in range(n+2)]
+        nextt = [0 for x in range(n+2)]
         for i in range(n-1,-1,-1):
             for time in range(n,0,-1):
-                take = time*sat[i] + dp[i+1][time+1]
-                not_take = dp[i+1][time]
-                dp[i][time] = max(take,not_take)
-        return dp[0][1]
+                take = time*sat[i] + nextt[time+1]
+                not_take = nextt[time]
+                curr[time] = max(take,not_take)
+            nextt = curr.copy()
+        return nextt[1]

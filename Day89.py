fb9d586498fe3e4a9c3f0544e80a1a56eb9c4178
Vansh <vanshgawra008@gmail.com>
2023-03-30 10:32:33 +0530

    Solved this using Memorization

---
+++ Day89.py
@@ -0,0 +1,25 @@
+# Problem : Scramble String
+# Problem Statement : We can scramble a string s to get a string t using the following algorithm:
+# If the length of the string is 1, stop.
+# If the length of the string is > 1, do the following:
+# Split the string into two non-empty substrings at a random index, i.e., if the string is s, divide it to x and y where s = x + y.
+# Randomly decide to swap the two substrings or to keep them in the same order. i.e., after this step, s may become s = x + y or s = y + x.
+# Apply step 1 recursively on each of the two substrings x and y.
+# Given two strings s1 and s2 of the same length, return true if s2 is a scrambled string of s1, otherwise, return false.
+class Solution:
+    def isScramble(self,s1, s2):
+        m ={}
+        def func(s1, s2):
+            if (s1, s2) in m:
+                return m[(s1, s2)]
+            if not sorted(s1) == sorted(s2):
+                return False
+            if len(s1) == 1:
+                return True
+            for i in range(1, len(s1)):
+                if func(s1[:i],s2[-i:]) and func(s1[i:],s2[:-i]) or func(s1[:i],s2[:i]) and func(s1[i:],s2[i:]):
+                    m[(s1, s2)] = True
+                    return True
+            m[(s1, s2)] = False
+            return False
+        return func(s1, s2)

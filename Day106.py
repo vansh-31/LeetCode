e31f3bdd846c3fec247b28820f64cdeee939e455
Vansh <vanshgawra008@gmail.com>
2023-04-16 07:10:36 +0530

    Solved usinmg DP

--- Day106.py
+++ Day106.py
@@ -10,3 +10,22 @@
 # Notice that you can use multiple characters from the same string in words provided the conditions above are met.
 
 # Return the number of ways to form target from words. Since the answer may be too large, return it modulo 109 + 7.
+class Solution:
+    def numWays(self, words: list[str], target: str) -> int:
+        n = len(words[0])
+        m = len(target)
+        mod = 10**9 + 7
+        dp = [0] * (m+1)
+        dp[0] = 1
+        
+        count = [[0] * 26 for _ in range(n)]
+        for i in range(n):
+            for word in words:
+                count[i][ord(word[i]) - ord('a')] += 1
+        
+        for i in range(n):
+            for j in range(m-1, -1, -1):
+                dp[j+1] += dp[j] * count[i][ord(target[j]) - ord('a')]
+                dp[j+1] %= mod
+        
+        return dp[m]

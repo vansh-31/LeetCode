22f79d65a763091866bb521fc341fe3e29b9b6da
Vansh <vanshgawra008@gmail.com>
2023-04-27 07:04:55 +0530

    Solved this using basic maths

---
+++ Day117.py
@@ -0,0 +1,8 @@
+# Problem : Bulb Switcher
+# Problem Statement : There are n bulbs that are initially off. You first turn on all the bulbs, then you turn off every second bulb.
+# On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the ith round, you toggle every i bulb. For the nth round, you only toggle the last bulb.
+# Return the number of bulbs that are on after n rounds.
+from math import isqrt
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return isqrt(n)

10b9b7c1e243f20a591555114645da9b8aed4c0e
Vansh <vanshgawra008@gmail.com>
2023-05-25 09:52:18 +0530

    Solved using DP

--- Day145.py
+++ Day145.py
@@ -4,3 +4,26 @@
 # Alice stops drawing numbers when she gets k or more points.
 # Return the probability that Alice has n or fewer points.
 # Answers within 10^-5 of the actual answer are considered accepted.
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        if k == 0 or n >= k + maxPts:
+            return 1.0
+        
+        windowSum = 1.0
+        probability = 0.0
+        
+        dp = [0.0] * (n + 1)
+        dp[0] = 1.0
+        
+        for i in range(1, n + 1):
+            dp[i] = windowSum / maxPts
+            
+            if i < k:
+                windowSum += dp[i]
+            else:
+                probability += dp[i]
+            
+            if i >= maxPts:
+                windowSum -= dp[i - maxPts]
+        
+        return probability

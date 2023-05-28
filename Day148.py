24124de06dae6999ff506086259806ac582df712
Vansh <vanshgawra008@gmail.com>
2023-05-28 15:44:40 +0530

    Solved using DP

--- Day148.py
+++ Day148.py
@@ -5,7 +5,7 @@
 # The cost of one cut is the length of the stick to be cut, the total cost is the sum of costs of all cuts. When you cut a stick, it will be split into two smaller sticks (i.e. the sum of their lengths is the length of the stick before the cut). Please refer to the first example for a better explanation.
 # Return the minimum total cost of the cuts.
 class Solution:
-    def minCost(self, n: int, cuts: List[int]):
+    def minCost(self, n: int, cuts):
         cuts.append(0)
         cuts.append(n)
         cuts.sort()

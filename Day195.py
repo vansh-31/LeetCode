8a000f1589107eb073a79f66634bf1aedae33d18
Vansh <vanshgawra008@gmail.com>
2023-07-14 18:55:16 +0530

    Solved using Tabulation

--- Day195.py
+++ Day195.py
@@ -1,6 +1,7 @@
 # Problem : Longest Arithmetic Subsequence of Given Difference
 # Problem Statement : Given an integer array arr and an integer difference, return the length of the longest subsequence in arr which is an arithmetic sequence such that the difference between adjacent elements in the subsequence equals difference.
 # A subsequence is a sequence that can be derived from arr by deleting some or no elements without changing the order of the remaining elements.
+from typing import List
 class Solution:
     def longestSubsequence(self, arr: List[int], difference: int) -> int:
         n = len(arr)

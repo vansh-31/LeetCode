f6a4d698d15699fd9f0873ce1e10b35a65f40db9
Vansh <vanshgawra008@gmail.com>
2023-07-21 16:32:04 +0530

    Solved using DP

---
+++ Day202.py
@@ -0,0 +1,21 @@
+# Problem : Number of Longest Increasing Subsequence
+# Problem Statement : Given an integer array nums, return the number of longest increasing subsequences.
+# Notice that the sequence has to be strictly increasing.
+from typing import List
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        n=len(nums)
+        dp=[1]*n
+        count=[1]*n
+        for i in range(1,n):
+            for j in range(i):
+                if nums[i]>nums[j]:
+                    if 1+dp[j]>dp[i]:
+                        dp[i]=dp[j]+1
+
+                        count[i]=count[j]
+
+                    elif dp[j]+1==dp[i]:
+                        count[i]+=count[j]
+        longest_len=max(dp)
+        return sum([count[i] for i in range(n) if dp[i]==longest_len])                

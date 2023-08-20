aa38c506ef8606f07d1df4298c41ec6a7fab4578
Vansh Gawra <vanshgawra008@gmail.com>
2023-08-20 07:06:49 +0530

    Solved using Topological Sort

--- Day232.py
+++ Day232.py
@@ -1,2 +1,46 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Sort Items by Groups Respecting Dependencies
+# Problem Statement : There are n items each belonging to zero or one of m groups where group[i] is the group that the i-th item belongs to and it's equal to -1 if the i-th item belongs to no group. The items and the groups are zero indexed. A group can have no item belonging to it.
+# Return a sorted list of the items such that:
+# The items that belong to the same group are next to each other in the sorted list.
+# There are some relations between these items where beforeItems[i] is a list containing all the items that should come before the i-th item in the sorted array (to the left of the i-th item).
+# Return any solution if there is more than one solution and return an empty list if there is no solution.
+from typing import List
+from collections import defaultdict, deque
+class Solution:
+    def sortItems(self, n: int, m: int, group: List[int], beforeItems: List[List[int]]) -> List[int]:
+        def topologicalSort(nodes, pred, succ):
+            order, no_pred = [], deque(node for node in nodes if not pred[node])
+            while no_pred:
+                node = no_pred.popleft()
+                order.append(node)
+                for s in succ[node]:
+                    pred[s].discard(node)
+                    if not pred[s]:
+                        no_pred.append(s)
+            return order if len(order) == len(nodes) else []
+        
+        group_to_items, new_group = defaultdict(set), m
+        for item in range(n):
+            if group[item] == -1: group[item] = new_group; new_group += 1
+            group_to_items[group[item]].add(item)
+        
+        intra_pred, intra_succ, inter_pred, inter_succ = defaultdict(set), defaultdict(set), defaultdict(set), defaultdict(set)
+        for item in range(n):
+            for before in beforeItems[item]:
+                if group[item] == group[before]:
+                    intra_pred[item].add(before)
+                    intra_succ[before].add(item)
+                else:
+                    inter_pred[group[item]].add(group[before])
+                    inter_succ[group[before]].add(group[item])
+        
+        groups_order = topologicalSort(list(group_to_items.keys()), inter_pred, inter_succ)
+        if not groups_order: return []
+        
+        items_order = []
+        for grp in groups_order:
+            order = topologicalSort(group_to_items[grp], intra_pred, intra_succ)
+            if not order: return []
+            items_order.extend(order)
+        
+        return items_order

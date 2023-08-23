f44b38788bd51b19c73b8569b0484c0cb0568ee4
Vansh Gawra <vanshgawra008@gmail.com>
2023-08-23 20:02:42 +0530

    Solved using Heap

--- Day235.py
+++ Day235.py
@@ -1,2 +1,33 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Reorganize String
+# Problem Statement : Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+# Return any possible rearrangement of s or return "" if not possible.
+import heapq
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        freq_map = {}
+        for char in s:
+            freq_map[char] = freq_map.get(char, 0) + 1
+            
+        max_heap = [(-freq, char) for char, freq in freq_map.items()]
+        heapq.heapify(max_heap)
+        
+        res = []
+        
+        while len(max_heap) >= 2:
+            freq1, char1 = heapq.heappop(max_heap)
+            freq2, char2 = heapq.heappop(max_heap)
+            
+            res.extend([char1, char2])
+            
+            if freq1 + 1 < 0:
+                heapq.heappush(max_heap, (freq1 + 1, char1))
+            if freq2 + 1 < 0:
+                heapq.heappush(max_heap, (freq2 + 1, char2))
+                
+        if max_heap:
+            freq, char = heapq.heappop(max_heap)
+            if -freq > 1:
+                return ""
+            res.append(char)
+            
+        return "".join(res)

a9101fbe3db62c11f63a0a51c65a9a308bc8947c
Vansh Gawra <vanshgawra008@gmail.com>
2023-09-05 18:32:41 +0530

    Solved in Linear Time and Space

--- Day248.py
+++ Day248.py
@@ -1,2 +1,51 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Copy List with Random Pointer
+# Problem Statement : A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
+# Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node. Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. None of the pointers in the new list should point to nodes in the original list.
+# For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding two nodes x and y in the copied list, x.random --> y.
+# Return the head of the copied linked list.
+# The linked list is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+# val: an integer representing Node.val
+# random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
+# Your code will only be given the head of the original linked list.
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next=None, random=None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+from typing import Optional
+
+
+class Solution:
+    def copyRandomList(self, head: Optional[Node]) -> Optional[Node]:
+        if head is None:
+            return None
+        HEAD = Node(head.val)
+        curr1, curr2 = head.next, HEAD
+        while curr1:
+            curr2.next = Node(curr1.val)
+            curr2 = curr2.next
+            curr1 = curr1.next
+        curr1, curr2 = head, HEAD
+        while curr1:
+            next1 = curr1.next
+            next2 = curr2.next # type: ignore
+            curr1.next = curr2
+            curr2.next = next1 # type: ignore
+            curr1 = next1
+            curr2 = next2
+        temp = head
+        while temp and temp.next:
+            if temp.random:
+                temp.next.random = temp.random.next
+            temp = temp.next.next
+        curr1, curr2 = head, head.next
+        while curr1 and curr2:
+            curr1.next = curr2.next
+            curr1 = curr1.next
+            if curr1:
+                curr2.next = curr1.next
+            curr2 = curr2.next
+        return HEAD

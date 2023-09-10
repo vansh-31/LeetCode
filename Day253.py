b12d75a2768ca790f0232f6cf9b63fe10e193415
Vansh Gawra <vanshgawra008@gmail.com>
2023-09-10 10:47:52 +0530

    Solved using Maths

--- Day253.py
+++ Day253.py
@@ -1,2 +1,12 @@
 # Problem : 
-# Problem Statement : 
+# Problem Statement : Given n orders, each order consist in pickup and delivery services. 
+# Count all valid pickup/delivery possible sequences such that delivery(i) is always after of pickup(i). 
+# Since the answer may be too large, return it modulo 10^9 + 7.
+class Solution:
+    def countOrders(self, n: int) -> int:
+        def fact(n):
+            x = 1
+            for i in range(2,n+1):
+                x *= i
+            return x
+        return (fact( n<<1 ) // (1<<n) ) % (10**9+7)

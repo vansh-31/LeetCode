d30a5ea809e1dd289de9f2c227a390e6bff4600f
Vansh Gawra <vanshgawra008@gmail.com>
2023-09-13 09:18:25 +0530

    Solved using Greedy Approach

--- Day256.py
+++ Day256.py
@@ -1,2 +1,23 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Candy
+# Problem Statement : There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
+# You are giving candies to these children subjected to the following requirements:
+# Each child must have at least one candy.
+# Children with a higher rating get more candies than their neighbors.
+# Return the minimum number of candies you need to have to distribute the candies to the children.
+from typing import List
+
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        candies = [1] * n
+
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
+        return sum(candies)

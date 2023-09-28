2aef007d6d41e4cefca2250d2c0f5b178b26397a
Vansh Gawra <vanshgawra008@gmail.com>
2023-09-28 11:05:20 +0530

    Solved using inbuilt sort

--- Day271.py
+++ Day271.py
@@ -1,2 +1,6 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Sort Array By Parity
+# Problem Statement : Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+# Return any array that satisfies this condition.
+class Solution:
+    def sortArrayByParity(self, nums: list[int]) -> list[int]:
+        return sorted(nums,key=lambda x : x%2)

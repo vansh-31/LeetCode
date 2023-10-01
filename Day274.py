a3f11675f252df8e6b1a51f7130c00c43ae115f9
Vansh Gawra <vanshgawra008@gmail.com>
2023-10-01 08:00:44 +0530

    Solved using split and join functions

--- Day274.py
+++ Day274.py
@@ -1,2 +1,5 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Reverse Words in a String III
+# Problem Statement : Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join([x[::-1] for x in s.split()])

f025d422fcfb9dad3e03e9f84ade36562664e52f
Vansh Gawra <vanshgawra008@gmail.com>
2023-10-03 11:50:30 +0530

    Solved using Hash Table

--- Day276.py
+++ Day276.py
@@ -1,2 +1,29 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Number of Good Pairs
+# Problem Statement : Given an array of integers nums, return the number of good pairs.
+# A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+from collections import defaultdict
+from functools import cache
+from typing import List
+
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        @cache
+        def fact(x):
+            if x <= 1:
+                return 1
+            return x * fact(x - 1)
+
+        @cache
+        def Ncr(N, r):
+            return fact(N) // (fact(N - r) * fact(r))
+
+        vals = defaultdict(int)
+        for x in nums:
+            vals[x] += 1
+        ans = 0
+        for x in vals.values():
+            if x >= 2:
+                print(x, Ncr(x, 2))
+                ans += Ncr(x, 2)
+        return ans

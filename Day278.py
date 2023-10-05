77dbfb708e69f9583a309e77da33573f865db5bf
Vansh Gawra <vanshgawra008@gmail.com>
2023-10-05 08:05:48 +0530

    Solved Using Boyer-Moore Majority Voting Algorithm

--- Day278.py
+++ Day278.py
@@ -1,2 +1,28 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Majority Element II
+# Problem Statement : Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+
+        candidate1, candidate2, count1, count2 = 0, 1, 0, 0
+
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = num, 1
+            elif count2 == 0:
+                candidate2, count2 = num, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+
+        return [
+            num for num in (candidate1, candidate2) if nums.count(num) > len(nums) // 3
+        ]

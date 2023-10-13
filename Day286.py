4bb5f3ff5ea7b4fb5411a3851b2d4bb14a9b715c
vansh-31 <vanshgawra008@gmail.com>
2023-10-13 07:57:24 +0530

    Solved using Tabulation DP

--- Day286.py
+++ Day286.py
@@ -1,2 +1,11 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Min Cost Climbing Stairs
+# Problem Statement : You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+# You can either start from the step with index 0, or the step with index 1.
+# Return the minimum cost to reach the top of the floor.
+class Solution:
+    def minCostClimbingStairs(self, cost: list[int]) -> int:
+        n = len(cost)
+        dp = [0] * (n+2)
+        for index in range(n-1,-1,-1):
+            dp[index] = cost[index] + min(dp[index+1],dp[index+2])
+        return min(dp[0],dp[1])

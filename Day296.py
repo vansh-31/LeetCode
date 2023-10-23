6c4b383361543cc042c61ea60f452bd10f361990
vansh-31 <vanshgawra008@gmail.com>
2023-10-23 07:13:15 +0530

    Solved using Recursion

--- Day296.py
+++ Day296.py
@@ -1,2 +1,10 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Power of Four
+# Problem Statement : Given an integer n, return true if it is a power of four. Otherwise, return false.
+# An integer n is a power of four, if there exists an integer x such that n == 4x.
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n == 1:
+            return True
+        if n < 4 or n%4:
+            return False
+        return self.isPowerOfFour(n>>2)

18e8fb49f4c1437fb8687413d4c5e7292d6e42fe
vansh-31 <vanshgawra008@gmail.com>
2023-10-27 08:58:59 +0530

    Solved using DP (Tabulation)

--- Day300.py
+++ Day300.py
@@ -1,2 +1,25 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Longest Palindromic Substring
+# Problem Statement : Given a string s, return the longest palindromic substring in s.
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        resLen = 0
+        for i in range(len(s)):
+            # odd len palindromes
+            l,r = i,i
+            while l >=0 and r < len(s) and s[l]==s[r]:
+                if ( r - l + 1 ) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+            
+            # even len palindromes
+            l,r = i,i+1
+            while l >=0 and r < len(s) and s[l]==s[r]:
+                if ( r - l + 1 ) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+        return res

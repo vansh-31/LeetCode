3382628aec23ebb5482b522f49f00eb0b550afca
vansh-31 <vanshgawra008@gmail.com>
2023-10-28 07:35:33 +0530

    Solved using Bottom Up DP

--- Day301.py
+++ Day301.py
@@ -1,2 +1,25 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Count Vowels Permutation
+# Problem Statement : Given an integer n, your task is to count how many strings of length n can be formed under the following rules:
+# Each character is a lower case vowel ('a', 'e', 'i', 'o', 'u')
+# Each vowel 'a' may only be followed by an 'e'.
+# Each vowel 'e' may only be followed by an 'a' or an 'i'.
+# Each vowel 'i' may not be followed by another 'i'.
+# Each vowel 'o' may only be followed by an 'i' or a 'u'.
+# Each vowel 'u' may only be followed by an 'a'.
+# Since the answer may be too large, return it modulo 10^9 + 7.
+class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+        MOD = 10**9 + 7
+        
+        a, e, i, o, u = 1, 1, 1, 1, 1
+        
+        for _ in range(1, n):
+            a_next = e
+            e_next = (a + i) % MOD
+            i_next = (a + e + o + u) % MOD
+            o_next = (i + u) % MOD
+            u_next = a
+            
+            a, e, i, o, u = a_next, e_next, i_next, o_next, u_next
+        
+        return (a + e + i + o + u) % MOD

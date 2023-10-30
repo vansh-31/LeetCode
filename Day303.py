651a80f187363a30cad9fa01f0a3c1d5cf12cc50
vansh-31 <vanshgawra008@gmail.com>
2023-10-30 08:45:11 +0530

    Solved using inbuilt Sort

--- Day303.py
+++ Day303.py
@@ -1,2 +1,6 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Sort Integers by The Number of 1 Bits
+# Problem Statement : You are given an integer array arr. Sort the integers in the array in ascending order by the number of 1's in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
+# Return the array after sorting it.
+class Solution:
+    def sortByBits(self, arr: list[int]) -> list[int]:
+        return sorted(arr, key=lambda x: (bin(x).count("1"), x))

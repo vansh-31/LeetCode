3e14ee01e8240a16ba04e99113fa64035bb90117
vansh-31 <vanshgawra008@gmail.com>
2023-11-04 08:49:51 +0530

    Solved in Linear Time and constant space complexity

--- Day308.py
+++ Day308.py
@@ -1,2 +1,8 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Last Moment Before All Ants Fall Out of a Plank
+# Problem Statement : We have a wooden plank of the length n units. Some ants are walking on the plank, each ant moves with a speed of 1 unit per second. Some of the ants move to the left, the other move to the right.
+# When two ants moving in two different directions meet at some point, they change their directions and continue moving again. Assume changing directions does not take any additional time.
+# When an ant reaches one end of the plank at a time t, it falls out of the plank immediately.
+# Given an integer n and two integer arrays left and right, the positions of the ants moving to the left and the right, return the moment when the last ant(s) fall out of the plank.
+class Solution:
+    def getLastMoment(self, n: int, left: list[int], right: list[int]) -> int:
+        return max(max(left, default=0), n - min(right, default=n))

6cea56d91f9cafb5e0581acf18987cdf30f694a0
vansh-31 <vanshgawra008@gmail.com>
2023-11-17 21:50:50 +0530

    Solved using Sorting (Greedy Approach) & two pointers

--- Day321.py
+++ Day321.py
@@ -1,2 +1,15 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Minimize Maximum Pair Sum in Array
+# Problem Statement : The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in a list of pairs.z
+# For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
+# Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+# Each element of nums is in exactly one pair, and
+# The maximum pair sum is minimized.
+# Return the minimized maximum pair sum after optimally pairing up the elements.
+class Solution:
+    def minPairSum(self, nums: list[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        ans = 0
+        for i in range(n // 2):
+            ans = max(ans, nums[i] + nums[-1 - i])
+        return ans

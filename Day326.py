22dcce4307a64479798dbf2359be690f869247a2
vansh-31 <vanshgawra008@gmail.com>
2023-11-22 23:37:39 +0530

    Solved using Sorting

--- Day326.py
+++ Day326.py
@@ -1,2 +1,13 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Diagonal Traverse II
+# Problem Statement : Given a 2D integer array nums, return all elements of nums in diagonal order as shown in the below images.
+from typing import List
+
+
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        m = len(nums)
+        result = []
+        for i in range(m):
+            for j in range(len(nums[i])):
+                result.append((i + j, m - 1 - i, nums[i][j]))
+        return [num for _, _, num in sorted(result)]

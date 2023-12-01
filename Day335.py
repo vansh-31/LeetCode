743937e820775d8f2012d9b0d6ccf43f5673d820
vansh-31 <vanshgawra008@gmail.com>
2023-12-01 06:07:47 +0530

    Solved using inbuilt function

--- Day335.py
+++ Day335.py
@@ -1,2 +1,6 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Check If Two String Arrays are Equivalent
+# Problem Statement : Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+# A string is represented by an array if the array elements concatenated in order forms the string.
+class Solution:
+    def arrayStringsAreEqual(self, word1: list[str], word2: list[str]) -> bool:
+        return "".join(word1) == "".join(word2)

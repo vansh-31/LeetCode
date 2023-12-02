2b2ae6f9fccbb31284c9df3909bae12bf4640b56
vansh-31 <vanshgawra008@gmail.com>
2023-12-02 12:24:02 +0530

    Solved using count array

--- Day336.py
+++ Day336.py
@@ -1,2 +1,19 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Find Words That Can Be Formed by Characters
+# Problem Statement : You are given an array of strings words and a string chars.
+# A string is good if it can be formed by characters from chars (each character can only be used once).
+# Return the sum of lengths of all good strings in words.
+class Solution:
+    def countCharacters(self, words: list[str], chars: str) -> int:
+        count = [0] * 26
+        for ch in chars:
+            count[ord(ch) - 97] += 1
+        ans = 0
+        for word in words:
+            temp = [0] * 26
+            for ch in word:
+                temp[ord(ch) - 97] += 1
+                if temp[ord(ch) - 97] > count[ord(ch) - 97]:
+                    break
+            else:
+                ans += len(word)
+        return ans

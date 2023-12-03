6a5e28980bb108d1f41cd037c8063977fc8d595c
vansh-31 <vanshgawra008@gmail.com>
2023-12-03 11:06:28 +0530

    Solved using basic maths and geometry

--- Day337.py
+++ Day337.py
@@ -9,4 +9,4 @@
 # You are allowed to pass through points that appear later in the order, but these do not count as visits.
 class Solution:
     def minTimeToVisitAllPoints(self, points: list[list[int]]) -> int:
-        return sum([max(abs(points[i][0]-points[i-1][0]),abs(points[i][1])-points[i-1][1]) for i in range(1,len(points))])
+        return sum([max(abs(points[i][0]-points[i-1][0]),abs(points[i][1]-points[i-1][1])) for i in range(1,len(points))])

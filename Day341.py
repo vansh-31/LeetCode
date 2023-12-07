4fa11e350e63702608bb2ffe6272a9ebde9df445
vansh-31 <vanshgawra008@gmail.com>
2023-12-08 01:41:37 +0530

    Solved using Basic Logic

--- Day341.py
+++ Day341.py
@@ -1,2 +1,9 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Largest Odd Number in String
+# Problem Statement : You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+# A substring is a contiguous sequence of characters within a string.
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        for i in range(len(num) - 1, -1, -1):
+            if num[i] in "13579":
+                return num[: i + 1]
+        return ""

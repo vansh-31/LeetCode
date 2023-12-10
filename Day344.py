27e7919046a2d5d1d488d6dbe7af830fc5712319
vansh-31 <vanshgawra008@gmail.com>
2023-12-10 14:59:06 +0530

    Solved using List Comprehenssion

--- Day344.py
+++ Day344.py
@@ -1,2 +1,6 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Transpose Matrix
+# Problem Statement : Given a 2D integer array matrix, return the transpose of matrix.
+# The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+class Solution:
+    def transpose(self, matrix: list[list[int]]) -> list[list[int]]:
+        return [ [ matrix[j][i] for j in range(len(matrix)) ] for i in range(len(matrix[0])) ]

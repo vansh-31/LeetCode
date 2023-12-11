d6fbf1f8add03c92dded0dfff1d80279b279a3fa
vansh-31 <vanshgawra008@gmail.com>
2023-12-11 13:16:41 +0530

    Solved using basic logic

--- Day345.py
+++ Day345.py
@@ -1,2 +1,16 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Element Appearing More Than 25% In Sorted Array
+# Problem Statement : Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
+class Solution:
+    def findSpecialInteger(self, arr: list[int]) -> int:
+        n = len(arr)
+        ans =  arr[0]
+        count = 0
+        for x in arr:
+            if x != ans:
+                ans = x
+                count = 1
+            else:
+                count += 1
+            if count > n/4:
+                return ans
+        return -1

69d637f83a8591b657106c663647c9e7feb2fe88
vansh-31 <vanshgawra008@gmail.com>
2023-12-12 19:49:46 +0530

    Solved using Heap

--- Day346.py
+++ Day346.py
@@ -1,2 +1,10 @@
-# Problem : 
-# Problem Statement : 
+# Problem :
+# Problem Statement : Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+from heapq import nlargest
+from math import prod
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        return prod(x - 1 for x in nlargest(2, nums))

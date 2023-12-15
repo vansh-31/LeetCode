a5e3d100dbb86c71e92c2ba02037969cbfc65f28
vansh-31 <vanshgawra008@gmail.com>
2023-12-15 10:10:21 +0530

    Solved using hashmap

--- Day349.py
+++ Day349.py
@@ -1,2 +1,14 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Destination City
+# Problem Statement : You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
+# It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+from collections import defaultdict
+from typing import List
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str: # type: ignore
+        d, p = defaultdict(str), set()
+        for i, j in paths:
+            d[i] += j
+            p.add(i)
+            p.add(j)
+        for i in p:
+            if i not in d.keys(): return i

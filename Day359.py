fdf04a819678616a58bdf50f767b3b3e12e29f5b
vansh-31 <vanshgawra008@gmail.com>
2023-12-25 13:05:21 +0530

    Solved using DP

--- Day359.py
+++ Day359.py
@@ -1,2 +1,27 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Decode Ways
+# Problem Statement : A message containing letters from A-Z can be encoded into numbers using the following mapping:
+# 'A' -> "1"
+# 'B' -> "2"
+# ...
+# 'Z' -> "26"
+# To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+# "AAJF" with the grouping (1 1 10 6)
+# "KJF" with the grouping (11 10 6)
+# Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+# Given a string s containing only digits, return the number of ways to decode it.
+# The test cases are generated so that the answer fits in a 32-bit integer.
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        curr = next2 = 0
+        next1 = 1
+        for index in range(n - 1, -1, -1):
+            single = pair = 0
+            if s[index] != "0":
+                curr = next1
+                if index < n - 1 and 26 >= int(f"{s[index]}{s[index+1]}"):
+                    curr += next2
+            next2 = next1
+            next1 = curr
+            curr = 0
+        return next1

19af39578e2305e7e8b4d04ebc7ed189bf3302c1
vansh-31 <vanshgawra008@gmail.com>
2023-12-31 12:00:07 +0530

    Solved using Basic Logic

--- Day365.py
+++ Day365.py
@@ -1,2 +1,12 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Largest Substring Between Two Equal Characters
+# Problem Statement : Given a string s, return the length of the longest substring between two equal characters, excluding the two characters. If there is no such substring return -1.
+# A substring is a contiguous sequence of characters within a string.
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+        n = len(s)
+        ans = -1
+        for i in range(n):
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    ans = max(ans, j - i - 1)
+        return ans

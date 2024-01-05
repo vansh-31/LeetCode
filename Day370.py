075839cf2ea3344f8a3baa110524827d67cc8d1e
vansh-31 <vanshgawra008@gmail.com>
2024-01-05 21:57:31 +0530

    Solved using DP + Binary Search

--- Day370.py
+++ Day370.py
@@ -1,2 +1,17 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Longest Increasing Subsequence
+# Problem Statement : Given an integer array nums, return the length of the longest strictly increasing subsequence.
+from bisect import bisect_left
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = [nums[0]]
+        for num in nums:
+            if num > ans[-1]:
+                ans.append(num)
+            else:
+                ind = bisect_left(ans, num)
+                ans[ind] = num
+        return len(ans)

f1165b8c22716be66963847307caf3b3d69e357e
vansh-31 <vanshgawra008@gmail.com>
2024-01-17 13:21:16 +0530

    Solved using Hashmap

--- Day382.py
+++ Day382.py
@@ -1,2 +1,11 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Unique Number of Occurrences
+# Problem Statement : Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+from collections import defaultdict
+
+
+class Solution:
+    def uniqueOccurrences(self, arr: list[int]) -> bool:
+        d = defaultdict(int)
+        for num in arr:
+            d[num] += 1
+        return len(d.values()) == len(set(d.values()))

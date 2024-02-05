8f29944d9beda8411ef03224b0c45bef5f53ada9
vansh-31 <vanshgawra008@gmail.com>
2024-02-05 17:49:02 +0530

    Solved using Counting

--- Day401.py
+++ Day401.py
@@ -1,2 +1,11 @@
-# Problem : 
-# Problem Statement : 
+# Problem : First Unique Character in a String
+# Problem Statement : Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = [0] * 26
+        for ch in s:
+            count[ord(ch) - 97] += 1
+        for index, ch in enumerate(s):
+            if count[ord(ch) - 97] == 1:
+                return index
+        return -1

7b7479c4b3ef277b66ddf0f7fcab8f5087b3c076
vansh-31 <vanshgawra008@gmail.com>
2024-02-06 14:31:05 +0530

    Solved using Hashmaps and Sorting

--- Day402.py
+++ Day402.py
@@ -1,2 +1,16 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Group Anagrams
+# Problem Statement : Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            anagram_map[sorted_word].append(word)
+
+        return list(anagram_map.values())

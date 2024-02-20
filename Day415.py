e33d5cc065f5e3ed22a543c13cb03840c8724ad7
vansh-31 <vanshgawra008@gmail.com>
2024-02-20 06:47:48 +0530

    Solved using XOR operator

--- Day415.py
+++ Day415.py
@@ -1,2 +1,10 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Missing Number
+# Problem Statement : Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+class Solution:
+    def missingNumber(self, nums: list[int]) -> int:
+        ans = 0
+        for num in nums:
+            ans ^= num
+        for x in range(len(nums) + 1):
+            ans ^= x
+        return ans

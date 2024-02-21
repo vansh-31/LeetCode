1b0c5e89fd252bf6a006d66cdb40ebc0e6de7d98
vansh-31 <vanshgawra008@gmail.com>
2024-02-21 10:18:36 +0530

    Solved using Bit Manipulation

--- Day416.py
+++ Day416.py
@@ -1,2 +1,7 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Bitwise AND of Numbers Range
+# Problem Statement : Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        while right > left:
+            right &= right - 1
+        return right

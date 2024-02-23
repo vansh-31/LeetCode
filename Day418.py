096b0851ca2e9499973787d2b96f6fccbca284c2
vansh-31 <vanshgawra008@gmail.com>
2024-02-23 20:24:19 +0530

    Solved using BFS

--- Day418.py
+++ Day418.py
@@ -1,2 +1,40 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Cheapest Flights Within K Stops
+# Problem Statement : There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
+# You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.
+from collections import defaultdict
+from queue import Queue
+from typing import List
+
+
+class Solution:
+    def findCheapestPrice(
+        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
+    ):
+        adj = defaultdict(list)
+        for flight in flights:
+            adj[flight[0]].append((flight[1], flight[2]))
+
+        dist = [float("inf")] * n
+        dist[src] = 0
+
+        q = Queue()
+        q.put((src, 0))
+        stops = 0
+
+        while not q.empty() and stops <= k:
+            sz = q.qsize()
+            for _ in range(sz):
+                node, distance = q.get()
+
+                if node not in adj:
+                    continue
+
+                for neighbour, price in adj[node]:
+                    if price + distance >= dist[neighbour]:
+                        continue
+                    dist[neighbour] = price + distance
+                    q.put((neighbour, dist[neighbour]))
+
+            stops += 1
+
+        return dist[dst] if dist[dst] != float("inf") else -1

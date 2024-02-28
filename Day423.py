4849c25448a23f2f2d92e14a902ae5b31955f1ce
vansh-31 <vanshgawra008@gmail.com>
2024-02-28 16:28:38 +0530

    Solved using BFS

--- Day423.py
+++ Day423.py
@@ -1,2 +1,23 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Find Bottom Left Tree Value
+# Problem Statement : Given the root of a binary tree, return the leftmost value in the last row of the tree.
+# Definition for a binary tree node.
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findBottomLeftValue(self, root: TreeNode) -> int:
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            if node.right:
+                q.append(node.right)
+            if node.left:
+                q.append(node.left)
+        return node.val

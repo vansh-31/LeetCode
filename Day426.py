0c197457ba64451e44944aa8b03918b477a9a19b
vansh-31 <vanshgawra008@gmail.com>
2024-03-02 09:15:20 +0530

    Solved using Sorting

--- Day426.py
+++ Day426.py
@@ -1,2 +1,5 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Squares of a Sorted Array
+# Problem Statement : Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+class Solution:
+    def sortedSquares(self, nums: list[int]) -> list[int]:
+        return sorted(x**2 for x in nums)

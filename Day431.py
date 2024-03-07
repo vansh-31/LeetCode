6b6b1b17e8b6dc01edb3ca5d35e47f5ba0a41fc0
vansh-31 <vanshgawra008@gmail.com>
2024-03-07 13:00:08 +0530

    Solved using slow and fast pointer approach

--- Day431.py
+++ Day431.py
@@ -1,2 +1,20 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Middle of the Linked List
+# Problem Statement : Given the head of a singly linked list, return the middle node of the linked list.
+# If there are two middle nodes, return the second middle node.
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next  # type: ignore
+            fast = fast.next.next
+        return slow

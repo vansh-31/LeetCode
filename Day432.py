8c0bace9beebd14a177ce4885f0c419ae01b27f0
vansh-31 <vanshgawra008@gmail.com>
2024-03-09 08:58:33 +0530

    Solved using Hashmap

--- Day432.py
+++ Day432.py
@@ -1,2 +1,17 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Count Elements With Maximum Frequency
+# Problem Statement : You are given an array nums consisting of positive integers.
+# Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+# The frequency of an element is the number of occurrences of that element in the array.
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        c = Counter(nums)
+        li = [i for i in c.values()]
+        m, res = max(li), 0
+        for i in li:
+            if i == m:
+                res += i
+        return res

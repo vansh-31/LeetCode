8c5f64da76e9af6e5860e739f5c511ac5c536405
vansh-31 <vanshgawra008@gmail.com>
2024-03-12 07:59:21 +0530

    Solved using Sorting and Two Pointers

--- Day434.py
+++ Day434.py
@@ -1,2 +1,22 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Intersection of Two Arrays
+# Problem Statement : Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+class Solution:
+    def intersection(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        nums1.sort()
+        nums2.sort()
+        n, m = len(nums1), len(nums2)
+        ans = []
+        i = j = 0
+        while i < n and j < m:
+            x, y = nums1[i], nums2[j]
+            if x == y:
+                ans.append(x)
+                while i < n and nums1[i] == x:
+                    i += 1
+                while j < m and nums2[j] == y:
+                    j += 1
+            if x < y:
+                i += 1
+            if y < x:
+                j += 1
+        return ans

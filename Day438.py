dd0544b66d05a99dc61521fdcee9f0ed946942f8
vansh-31 <vanshgawra008@gmail.com>
2024-03-13 21:42:41 +0530

    Solved using Prefix Sum

--- Day438.py
+++ Day438.py
@@ -1,2 +1,11 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Find the Pivot Integer
+# Problem Statement : Given a positive integer n, find the pivot integer x such that:
+# The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
+# Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most one pivot index for the given input.
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        li,c=[i for i in range(1,n+1)],-1
+        for i in range(len(li)):
+            if sum(li[0:i+1])==sum(li[i:]):
+                c=i+1
+        return c

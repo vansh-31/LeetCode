04fc7d886f0d70e6ea9f3c719c57e3454eddb9ec
vansh-31 <vanshgawra008@gmail.com>
2024-03-18 09:31:21 +0530

    Solved using Greedy Approach

--- 2nd_Streak/Day2.py
+++ 2nd_Streak/Day2.py
@@ -1,2 +1,19 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Minimum Number of Arrows to Burst Balloons
+# Problem Statement : There are some spherical balloons taped onto a flat wall that represents the XY-plane. The balloons are represented as a 2D integer array points where points[i] = [xstart, xend] denotes a balloon whose horizontal diameter stretches between xstart and xend. You do not know the exact y-coordinates of the balloons.
+# Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis. A balloon with xstart and xend is burst by an arrow shot at x if xstart <= x <= xend. There is no limit to the number of arrows that can be shot. A shot arrow keeps traveling up infinitely, bursting any balloons in its path.
+# Given the array points, return the minimum number of arrows that must be shot to burst all balloons.
+class Solution:
+    def findMinArrowShots(self, points):
+        if not points:
+            return 0
+        arrows = 1
+        points.sort()
+        start, end = points[0]
+        for i in range(1, len(points)):
+            if points[i][0] >= start and points[i][0] <= end:
+                end = min(end, points[i][1])
+            else:
+                arrows += 1
+                end = points[i][1]
+            start = points[i][0]
+        return arrows

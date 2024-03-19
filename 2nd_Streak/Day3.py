158f4bd9b13b93ececb3c5896dda4b8a8548ea24
vansh-31 <vanshgawra008@gmail.com>
2024-03-19 09:49:08 +0530

    Solved using Greedy Approach

--- 2nd_Streak/Day3.py
+++ 2nd_Streak/Day3.py
@@ -1,2 +1,16 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Task Scheduler
+# Problem Statement : You are given an array of CPU tasks, each represented by letters A to Z, and a cooling time, n. Each cycle or interval allows the completion of one task. Tasks can be completed in any order, but there's a constraint: identical tasks must be separated by at least n intervals due to cooling time.
+# ​Return the minimum number of intervals required to complete all tasks.
+class Solution:
+    def leastInterval(self, tasks: list[str], n: int) -> int:
+        freq = [0] * 26
+        for task in tasks:
+            freq[ord(task) - ord("A")] += 1
+        freq.sort()
+        chunk = freq[25] - 1
+        idle = chunk * n
+
+        for i in range(24, -1, -1):
+            idle -= min(chunk, freq[i])
+
+        return len(tasks) + idle if idle >= 0 else len(tasks)

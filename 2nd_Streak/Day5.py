b2729cf3ac3b7cb0c09f6d9c440a4fcfc988cb2e
vansh-31 <vanshgawra008@gmail.com>
2024-03-21 18:28:36 +0530

    Solved using Iterative approach

--- 2nd_Streak/Day5.py
+++ 2nd_Streak/Day5.py
@@ -1,2 +1,18 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Reverse Linked List
+# Problem Statement : Given the head of a singly linked list, reverse the list, and return the reversed list.
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: ListNode):
+        curr, prev = head, None
+        while curr:
+            nextt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextt
+        return prev

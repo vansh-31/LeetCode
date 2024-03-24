17dbb9d6ba9049c593ceb0a68cbc017ebdd4b345
vansh-31 <vanshgawra008@gmail.com>
2024-03-24 17:11:34 +0530

    Solved using slow & fast pointer approach

--- 2nd_Streak/Day8.py
+++ 2nd_Streak/Day8.py
@@ -1,2 +1,19 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Find the Duplicate Number
+# Problem Statement : Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+# There is only one repeated number in nums, return this repeated number.
+# You must solve the problem without modifying the array nums and uses only constant extra space.
+class Solution:
+    def findDuplicate(self, nums: list[int]) -> int:
+        n = len(nums)
+        slow = nums[0]
+        fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow

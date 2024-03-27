5868e43f535467eb8697ef015c9e62f31389b0d8
vansh-31 <vanshgawra008@gmail.com>
2024-03-27 23:53:07 +0530

    Solved using sliding window

--- 3rd_Streak/Day2.py
+++ 3rd_Streak/Day2.py
@@ -1,2 +1,19 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Subarray Product Less Than K
+# Problem Statement : Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: list[int], k: int) -> int:
+        if k <= 1:
+            return 0
+
+        left, right, product, count = 0, 0, 1, 0
+        n = len(nums)
+
+        while right < n:
+            product *= nums[right]
+            while product >= k:
+                product //= nums[left]
+                left += 1
+            count += 1 + (right - left)
+            right += 1
+
+        return count

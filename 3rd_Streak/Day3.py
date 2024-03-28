2458971ee0ec7b3c9721581c8233bd8cbad3318b
vansh-31 <vanshgawra008@gmail.com>
2024-03-28 18:26:22 +0530

    Solved using Sliding Window Approach

--- 3rd_Streak/Day3.py
+++ 3rd_Streak/Day3.py
@@ -1,2 +1,22 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Length of Longest Subarray With at Most K Frequency
+# Problem Statement : You are given an integer array nums and an integer k.
+# The frequency of an element x is the number of times it occurs in an array.
+# An array is called good if the frequency of each element in this array is less than or equal to k.
+# Return the length of the longest good subarray of nums.
+# A subarray is a contiguous non-empty sequence of elements within an array.
+class Solution:
+    def maxSubarrayLength(self, nums: list[int], k: int) -> int:
+        ans = 0
+        mp = {}
+        l = 0
+        n = len(nums)
+        for r in range(n):
+            mp[nums[r]] = mp.get(nums[r], 0) + 1
+            if mp[nums[r]] > k:
+                while nums[l] != nums[r]:
+                    mp[nums[l]] -= 1
+                    l += 1
+                mp[nums[l]] -= 1
+                l += 1
+            ans = max(ans, r - l + 1)
+        return ans

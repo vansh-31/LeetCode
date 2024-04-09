68a1ee47476e6becb03618e225890acdbee8a306
vansh-31 <vanshgawra008@gmail.com>
2024-04-09 18:55:23 +0530

    Solved using Simulation

--- 3rd_Streak/Day15.py
+++ 3rd_Streak/Day15.py
@@ -1,2 +1,18 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Time Needed to Buy Tickets
+# Problem Statement : There are n people in a line queuing to buy tickets, where the 0th person is at the front of the line and the (n - 1)th person is at the back of the line.
+# You are given a 0-indexed integer array tickets of length n where the number of tickets that the ith person would like to buy is tickets[i].
+# Each person takes exactly 1 second to buy a ticket. A person can only buy 1 ticket at a time and has to go back to the end of the line (which happens instantaneously) in order to buy more tickets. If a person does not have any tickets left to buy, the person will leave the line.
+# Return the time taken for the person at position k (0-indexed) to finish buying tickets.
+class Solution:
+    def timeRequiredToBuy(self, tickets: list[int], k: int) -> int:
+        c = 0
+        while True:
+            if tickets[k] == 0:
+                break
+            for i in range(len(tickets)):
+                if tickets[k] == 0:
+                    break
+                if tickets[i] > 0:
+                    tickets[i] -= 1
+                    c += 1
+        return c

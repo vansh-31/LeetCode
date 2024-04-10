9bd0369b20b7d726cd3eabdfd90ac7f239f1975c
vansh-31 <vanshgawra008@gmail.com>
2024-04-10 13:49:48 +0530

    Solved using Double Ended Queue

--- 3rd_Streak/Day16.py
+++ 3rd_Streak/Day16.py
@@ -1,2 +1,22 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Reveal Cards In Increasing Order
+# Problem Statement : You are given an integer array deck. There is a deck of cards where every card has a unique integer. The integer on the ith card is deck[i].
+# You can order the deck in any order you want. Initially, all the cards start face down (unrevealed) in one deck.
+# You will do the following steps repeatedly until all cards are revealed:
+# Take the top card of the deck, reveal it, and take it out of the deck.
+# If there are still cards in the deck then put the next top card of the deck at the bottom of the deck.
+# If there are still unrevealed cards, go back to step 1. Otherwise, stop.
+# Return an ordering of the deck that would reveal the cards in increasing order.
+# Note that the first entry in the answer is considered to be the top of the deck.
+from collections import deque
+
+
+class Solution:
+    def deckRevealedIncreasing(self, deck: list[int]) -> list[int]:
+        deck.sort(reverse=True)
+        res = deque()
+        for x in deck:
+            if res:
+                temp = res.pop()
+                res.appendleft(temp)
+            res.appendleft(x)
+        return res  # type: ignore

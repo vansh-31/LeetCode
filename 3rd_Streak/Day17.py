c3f7a0dbf32e4234bfc2eb1ee0e395c644ba1e79
vansh-31 <vanshgawra008@gmail.com>
2024-04-11 15:40:43 +0530

    Solved using Greedy Approach and stack

--- 3rd_Streak/Day17.py
+++ 3rd_Streak/Day17.py
@@ -1,2 +1,12 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Remove K Digits
+# Problem Statement : Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+        for d in num:
+            while stack and k and stack[-1] > d:
+                stack.pop()
+                k -= 1
+            stack.append(d)
+        res = stack[:-k] if k else stack
+        return "".join(res).lstrip("0") or "0"

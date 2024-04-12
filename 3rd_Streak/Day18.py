7f725a40390aa6ec89806add1f4d30e493119608
vansh-31 <vanshgawra008@gmail.com>
2024-04-12 20:38:29 +0530

    Solved using two pointers approach

--- 3rd_Streak/Day18.py
+++ 3rd_Streak/Day18.py
@@ -1,2 +1,20 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Trapping Rain Water
+# Problem Statement : Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+class Solution:
+    def trap(self, height: list[int]) -> int:
+        n = len(height)
+        leftBigger = height[:]
+        rightBigger = height[:]
+        mxLeft = height[0]
+        mxRight = height[-1]
+        for i in range(n):
+            if height[i] > mxLeft:
+                mxLeft = height[i]
+            if height[-1-i] > mxRight:
+                mxRight = height[-1-i]
+            leftBigger[i] = mxLeft
+            rightBigger[-1-i] = mxRight
+        ans = 0
+        for i in range(n):
+            ans += min( leftBigger[i], rightBigger[i] ) - height[i]
+        return ans

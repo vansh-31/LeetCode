842e8a65c028a9915dc10263588f4d682a3bb3ad
vansh-31 <vanshgawra008@gmail.com>
2024-04-18 20:53:35 +0530

    Solved using DFS

--- 3rd_Streak/Day24.py
+++ 3rd_Streak/Day24.py
@@ -1,2 +1,17 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Island Perimeter
+# Problem Statement : You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+# Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+# The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+class Solution:
+    def islandPerimeter(self, grid: list[list[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    ans += 4
+                    if i < m - 1 and grid[i + 1][j] == 1:
+                        ans -= 2
+                    if j < n - 1 and grid[i][j + 1] == 1:
+                        ans -= 2
+        return ans

a8f84f7ce0f1517b058b324bca667ed9658c8407
vansh-31 <vanshgawra008@gmail.com>
2024-04-19 11:22:43 +0530

    Solved using DFS

--- 3rd_Streak/Day25.py
+++ 3rd_Streak/Day25.py
@@ -1,2 +1,23 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Number of Islands
+# Problem Statement : Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+# An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+class Solution:
+    def numIslands(self, grid: list[list[str]]) -> int:
+        m, n = len(grid), len(grid[0])
+        ans = 0
+
+        def dfs(r, c):
+            if r < 0 or r == m or c < 0 or c == n or grid[r][c] == "0":
+                return
+            grid[r][c] = "0"
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c - 1)
+            dfs(r, c + 1)
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    dfs(i, j)
+                    ans += 1
+        return ans

85c0fbd4bf63745ea8d2d1cd044eb23b4e4708ca
vansh-31 <vanshgawra008@gmail.com>
2024-05-01 09:27:24 +0530

    Add solution for reversing prefix of word using index and slicing

--- 3rd_Streak/Day37.py
+++ 3rd_Streak/Day37.py
@@ -1,2 +1,10 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Reverse Prefix of Word
+# Problem Statement : Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
+# For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
+# Return the resulting string.
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        if ch not in word:
+            return word
+        l = word.index(ch)
+        return word[: l + 1][::-1] + word[l + 1 :]

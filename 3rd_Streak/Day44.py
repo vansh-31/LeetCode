494c4afd943130629faf72f97a3a443469acc2cb
vansh-31 <vanshgawra008@gmail.com>
2024-05-08 18:48:54 +0530

    Solved using Sorting

--- 3rd_Streak/Day44.py
+++ 3rd_Streak/Day44.py
@@ -1,2 +1,24 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Relative Ranks
+# Problem Statement : You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
+# The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on. The placement of each athlete determines their rank:
+# The 1st place athlete's rank is "Gold Medal".
+# The 2nd place athlete's rank is "Silver Medal".
+# The 3rd place athlete's rank is "Bronze Medal".
+# For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
+# Return an array answer of size n where answer[i] is the rank of the ith athlete.
+class Solution:
+    def findRelativeRanks(self, score: list[int]) -> list[str]:
+        sorted_score = sorted(score, reverse=True)
+        rank_map = {score: rank for rank, score in enumerate(sorted_score, 1)}
+        answer = []
+        for s in score:
+            rank = rank_map[s]
+            if rank == 1:
+                answer.append("Gold Medal")
+            elif rank == 2:
+                answer.append("Silver Medal")
+            elif rank == 3:
+                answer.append("Bronze Medal")
+            else:
+                answer.append(str(rank))
+        return answer

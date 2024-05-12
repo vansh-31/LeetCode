e1397093e7d162eb349cd54e2f127150a6f46664
vansh-31 <vanshgawra008@gmail.com>
2024-05-12 08:59:45 +0530

    Solved using max pool technique

--- 3rd_Streak/Day48.py
+++ 3rd_Streak/Day48.py
@@ -1,2 +1,18 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Largest Local Values in a Matrix
+# Problem Statement : You are given an n x n integer matrix grid.
+# Generate an integer matrix maxLocal of size (n - 2) x (n - 2) such that:
+# maxLocal[i][j] is equal to the largest value of the 3 x 3 matrix in grid centered around row i + 1 and column j + 1.
+# In other words, we want to find the largest value in every contiguous 3 x 3 matrix in grid.
+# Return the generated matrix.
+class Solution:
+    def largestLocal(self, grid: list[list[int]]) -> list[list[int]]:
+        res = []
+        n = len(grid)
+        for i in range(n - 2):
+            res.append([])
+            for j in range(n - 2):
+                max_val = max(
+                    grid[i][j : j + 3] + grid[i + 1][j : j + 3] + grid[i + 2][j : j + 3]
+                )
+                res[-1].append(max_val)
+        return res

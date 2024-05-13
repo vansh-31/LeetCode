64bd538a2dcdb008d65965f56ae52b2a22fc8255
vansh-31 <vanshgawra008@gmail.com>
2024-05-13 16:10:24 +0530

    Solved using Greedy approach

--- 3rd_Streak/Day49.py
+++ 3rd_Streak/Day49.py
@@ -1,2 +1,23 @@
 # Problem : 
 # Problem Statement : 
+class Solution:
+    def matrixScore(self, grid: list[list[int]]) -> int:
+        m,n = len(grid),len(grid[0])
+        cols = [0] * n
+        msb = True
+        for i in range(m):
+            if grid[i][0] == 0:
+                msb = False
+            for j in range(n):
+                if not msb:
+                    grid[i][j] = int(not grid[i][j])
+                cols[j] += grid[i][j]
+            msb = True
+        for col,val in enumerate(cols):
+            if val < m/2:
+                for row in grid:
+                    row[col] = int(not row[col])
+        ans = 0
+        for row in grid:
+            ans += sum([2**i * val for i,val in enumerate(row[::-1])])
+        return ans

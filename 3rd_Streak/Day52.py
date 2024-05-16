31f3e4b181ef1c5b4033d93995897c641a7a370e
vansh-31 <vanshgawra008@gmail.com>
2024-05-16 06:58:53 +0530

    Solved using Recursion

--- 3rd_Streak/Day52.py
+++ 3rd_Streak/Day52.py
@@ -1,2 +1,23 @@
-# Problem : 
-# Problem Statement : 
+# Problem :
+# Problem Statement :
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def evaluateTree(self, root: TreeNode) -> bool:
+        def solve(root):
+            if root.val <= 1:
+                return root.val == 1
+            left = solve(root.left)
+            right = solve(root.right)
+            if root.val == 2:
+                return left or right
+            else:
+                return left and right
+
+        return solve(root)

546e0ef57530106e408778d33111b50cb960da7d
vansh-31 <vanshgawra008@gmail.com>
2024-05-20 16:51:33 +0530

    Solved using Recursion

--- 3rd_Streak/Day56.py
+++ 3rd_Streak/Day56.py
@@ -1,2 +1,13 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Sum of All Subset XOR Totals
+# Problem Statement : The XOR total of an array is defined as the bitwise XOR of all its elements, or 0 if the array is empty.
+# For example, the XOR total of the array [2,5,6] is 2 XOR 5 XOR 6 = 1.
+# Given an array nums, return the sum of all XOR totals for every subset of nums. 
+# Note: Subsets with the same elements should be counted multiple times.
+# An array a is a subset of an array b if a can be obtained from b by deleting some (possibly zero) elements of b.
+class Solution:
+    def subsetXORSum(self, nums: list[int]) -> int:
+        def rec(index, x):
+            if index==len(nums):
+                return x
+            return rec(index+1,x)+rec(index+1,x^nums[index])
+        return rec(0,0)

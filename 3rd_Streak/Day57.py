ebad57137c36828531d02702f551dc545ee1ebe6
vansh-31 <vanshgawra008@gmail.com>
2024-05-21 11:32:50 +0530

    Solved using Recursion

--- 3rd_Streak/Day57.py
+++ 3rd_Streak/Day57.py
@@ -1,2 +1,18 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Subsets
+# Problem Statement : Given an integer array nums of unique elements, return all possible subsets (the power set).
+# The solution set must not contain duplicate subsets. Return the solution in any order.
+class Solution:
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        ans = set()
+
+        def solve(i, curr):
+            if i == len(nums):
+                ans.add(tuple(curr[:]))
+                return
+            curr.append(nums[i])
+            solve(i + 1, curr)
+            curr.pop()
+            solve(i + 1, curr)
+
+        solve(0, [])
+        return [list(x) for x in ans]

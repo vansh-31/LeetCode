98fb80b88e52a9ec5299a03f5bf3a0372bd7c47c
vansh-31 <vanshgawra008@gmail.com>
2024-05-30 06:57:26 +0530

    Solved using Bit Manipulation

--- 3rd_Streak/Day66.py
+++ 3rd_Streak/Day66.py
@@ -1,2 +1,18 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Count Triplets That Can Form Two Arrays of Equal XOR
+# Problem Statement : Given an array of integers arr.
+# We want to select three indices i, j and k where (0 <= i < j <= k < arr.length).
+# Let's define a and b as follows:
+# a = arr[i] ^ arr[i + 1] ^ ... ^ arr[j - 1]
+# b = arr[j] ^ arr[j + 1] ^ ... ^ arr[k]
+# Note that ^ denotes the bitwise-xor operation.
+# Return the number of triplets (i, j and k) Where a == b.
+class Solution:
+    def countTriplets(self, arr: list[int]) -> int:
+        ans = 0
+        for i in range(len(arr)):
+            x = arr[i]
+            for j in range(i + 1, len(arr)):
+                x ^= arr[j]
+                if x == 0:
+                    ans += j - i
+        return ans

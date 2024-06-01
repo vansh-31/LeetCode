4f75fe25f29bc13f9266aea4ea7f65dba44f8e8a
vansh-31 <vanshgawra008@gmail.com>
2024-06-01 15:30:30 +0530

    Solved using Basic Logic

--- 3rd_Streak/Day68.py
+++ 3rd_Streak/Day68.py
@@ -1,2 +1,6 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Score of a String
+# Problem Statement : You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+# Return the score of s.
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        return sum([abs(ord(s[i]) - ord(s[i - 1])) for i in range(1, len(s))])

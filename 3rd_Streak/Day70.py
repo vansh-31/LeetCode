902a3b041e5c094b7a3f90b87360f183d9b797f1
vansh-31 <vanshgawra008@gmail.com>
2024-06-03 17:31:29 +0530

    Solved using two pointers approach

--- 3rd_Streak/Day70.py
+++ 3rd_Streak/Day70.py
@@ -1,2 +1,15 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Append Characters to String to Make Subsequence
+# Problem Statement : You are given two strings s and t consisting of only lowercase English letters.
+# Return the minimum number of characters that need to be appended to the end of s so that t becomes a subsequence of s.
+# A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        n, m = len(s), len(t)
+        i = j = 0
+        while i < n and j < m:
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                i += 1
+        return m - j

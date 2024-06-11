00fdec54567aecb20bb66b1cf589c5b40c9027c4
vansh-31 <vanshgawra008@gmail.com>
2024-06-11 14:45:56 +0530

    Solved using Counting Sort

--- 3rd_Streak/Day78.py
+++ 3rd_Streak/Day78.py
@@ -1,2 +1,20 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Relative Sort Array
+# Problem Statement : Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+# Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
+class Solution:
+    def relativeSortArray(self, arr1: list[int], arr2: list[int]) -> list[int]:
+        freq = [0] * (max(arr1) + 1)
+        for num in arr1:
+            freq[num] += 1
+        i = 0
+        for num in arr2:
+            while freq[num]:
+                arr1[i] = num
+                freq[num] -= 1
+                i += 1
+        for j, num in enumerate(freq):
+            while num:
+                arr1[i] = j
+                num -= 1
+                i += 1
+        return arr1

09a2c3e6409142560e14dc04d08a32db5396acac
vansh-31 <vanshgawra008@gmail.com>
2024-06-13 06:41:54 +0530

    Solved using Sorting

--- 3rd_Streak/Day80.py
+++ 3rd_Streak/Day80.py
@@ -1,2 +1,14 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Minimum Number of Moves to Seat Everyone
+# Problem Statement : There are n seats and n students in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat. You are also given the array students of length n, where students[j] is the position of the jth student.
+# You may perform the following move any number of times:
+# Increase or decrease the position of the ith student by 1 (i.e., moving the ith student from position x to x + 1 or x - 1)
+# Return the minimum number of moves required to move each student to a seat such that no two students are in the same seat.
+# Note that there may be multiple seats or students in the same position at the beginning.
+class Solution:
+    def minMovesToSeat(self, seats: list[int], students: list[int]) -> int:
+        seats.sort()
+        students.sort()
+        moves = 0
+        for seat, student in zip(seats, students):
+            moves += abs(seat - student)
+        return moves

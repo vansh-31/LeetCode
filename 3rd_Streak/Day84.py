a2a07ca482ee3bd5d352e7caac55e77a3937e20e
vansh-31 <vanshgawra008@gmail.com>
2024-06-18 14:31:43 +0530

    Solved using Two Pointers & Binary Search Approach

--- 3rd_Streak/Day84.py
+++ 3rd_Streak/Day84.py
@@ -1,2 +1,14 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Sum of Square Numbers
+# Problem Statement : Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        low, high = 0, int(c**0.5) + 1
+        while low <= high:
+            mid = (low**2) + (high**2)
+            if mid == c:
+                return True
+            if mid > c:
+                high -= 1
+            else:
+                low += 1
+        return False

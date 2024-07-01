3b15de781671b0c9a3e786f1d5203fd990e643b7
vansh-31 <vanshgawra008@gmail.com>
2024-07-01 14:22:19 +0530

    Solved using Counting

--- 3rd_Streak/Day98.py
+++ 3rd_Streak/Day98.py
@@ -1,2 +1,13 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Three Consecutive Odds
+# Problem Statement : Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+class Solution:
+    def threeConsecutiveOdds(self, arr: list[int]) -> bool:
+        count = 0
+        for x in arr:
+            if x & 1:
+                count += 1
+                if count == 3:
+                    return True
+            else:
+                count = 0
+        return False

1c1b686ebe894bce33c20f33fe049696ffc9cd9f
vansh-31 <vanshgawra008@gmail.com>
2024-07-11 20:08:22 +0530

    Solved using Stack

--- 3rd_Streak/Day108.py
+++ 3rd_Streak/Day108.py
@@ -1,2 +1,18 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Reverse Substrings Between Each Pair of Parentheses
+# Problem Statement : You are given a string s that consists of lower case English letters and brackets.
+# Reverse the strings in each pair of matching parentheses, starting from the innermost one.
+# Your result should not contain any brackets.
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = []
+        ans = ""
+        for ch in s:
+            if ch == "(":
+                stack.append(ans)
+                ans = ""
+            elif ch == ")":
+                ans = ans[::-1]
+                ans = stack.pop() + ans
+            else:
+                ans += ch
+        return ans

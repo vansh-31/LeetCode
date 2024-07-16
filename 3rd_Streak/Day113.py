6efebbd937050e25f57645410650ca7ffbe91c30
vansh-31 <vanshgawra008@gmail.com>
2024-07-16 06:37:48 +0530

    Solved using Recursion

--- 3rd_Streak/Day113.py
+++ 3rd_Streak/Day113.py
@@ -1,2 +1,33 @@
-# Problem : 
-# Problem Statement : 
+# Problem :
+# Problem Statement :
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from typing import List, Optional
+
+
+class Solution:
+    def getDirections(
+        self, root: Optional[TreeNode], startValue: int, destValue: int
+    ) -> str:
+        def find(n: TreeNode, val: int, path: List[str]) -> bool:
+            if n.val == val:
+                return True
+            if n.left and find(n.left, val, path):
+                path += "L"
+            elif n.right and find(n.right, val, path):
+                path += "R"
+            return path  # type: ignore
+
+        s, d = [], []
+        find(root, startValue, s)  # type: ignore
+        find(root, destValue, d)  # type: ignore
+        while len(s) and len(d) and s[-1] == d[-1]:
+            s.pop()
+            d.pop()
+        return "".join("U" * len(s)) + "".join(reversed(d))

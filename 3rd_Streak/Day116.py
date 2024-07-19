9035c121ad20da2b77f85411fc3903720976a481
Vansh Gawra <vanshgawra008@gmail.com>
2024-07-19 20:27:31 +0530

    Solved using basic logic

--- 3rd_Streak/Day116.py
+++ 3rd_Streak/Day116.py
@@ -1,2 +1,11 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Lucky Numbers in a Matrix
+# Problem Statement : Given an m x n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
+# A lucky number is an element of the matrix such that it is the minimum element in its row and maximum in its column.
+class Solution:
+    def luckyNumbers(self, matrix: list[list[int]]) -> list[int]:
+        colMax = set(map(max, zip(*matrix)))
+        for row in matrix:
+            mn = min(row)
+            if mn in colMax:
+                return [mn]
+        return []

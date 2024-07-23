7f5a76135b3c4e49f944a5f435fed90aa16ea743
Vansh Gawra <vanshgawra008@gmail.com>
2024-07-23 18:46:15 +0530

    Solved using Hashmap and sorting

--- 3rd_Streak/Day120.py
+++ 3rd_Streak/Day120.py
@@ -1,2 +1,11 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Sort Array by Increasing Frequency
+# Problem Statement : Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+# Return the sorted array.
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        freq = Counter(nums)
+        return sorted(nums, key=lambda x: (freq[x], -x))

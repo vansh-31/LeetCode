9aa63a2f40db735a51930a1702bfcd22af402789
Vansh Gawra <vanshgawra008@gmail.com>
2024-07-29 14:01:11 +0530

    Solved using DP

--- 3rd_Streak/Day126.py
+++ 3rd_Streak/Day126.py
@@ -1,2 +1,28 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Count Number of Teams
+# Problem Statement : There are n soldiers standing in a line. Each soldier is assigned a unique rating value.
+# You have to form a team of 3 soldiers amongst them under the following rules:
+# Choose 3 soldiers with index (i, j, k) with rating (rating[i], rating[j], rating[k]).
+# A team is valid if: (rating[i] < rating[j] < rating[k]) or (rating[i] > rating[j] > rating[k]) where (0 <= i < j < k < n).
+# Return the number of teams you can form given the conditions. (soldiers can be part of multiple teams).
+class Solution:
+    def numTeams(self, rating: list[int]) -> int:
+        n = len(rating)
+        count = 0
+        for j in range(n):
+            leftLess = leftGreater = rightLess = rightGreater = 0
+
+            for i in range(j):
+                if rating[i] < rating[j]:
+                    leftLess += 1
+                elif rating[i] > rating[j]:
+                    leftGreater += 1
+
+            for k in range(j + 1, n):
+                if rating[k] < rating[j]:
+                    rightLess += 1
+                elif rating[k] > rating[j]:
+                    rightGreater += 1
+
+            count += leftLess * rightGreater + leftGreater * rightLess
+
+        return count

7ff557f07120864c961e8e71c03a6e875de95e13
Vansh Gawra <vanshgawra008@gmail.com>
2024-07-30 11:46:58 +0530

    Solved using DP

--- 3rd_Streak/Day127.py
+++ 3rd_Streak/Day127.py
@@ -1,2 +1,13 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Minimum Deletions to Make String Balanced
+# Problem Statement : You are given a string s consisting only of characters 'a' and 'b'​​​​.
+# You can delete any number of characters in s to make s balanced. s is balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
+# Return the minimum number of deletions needed to make s balanced.
+class Solution(object):
+    def minimumDeletions(self, s):
+        ans, countB = 0, 0
+        for ch in s:
+            if ch == "a":
+                ans = min(ans + 1, countB)
+            else:
+                countB += 1
+        return ans

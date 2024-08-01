35cfbe81b3988dc62821633e5ae9f5690e39a577
Vansh Gawra <vanshgawra008@gmail.com>
2024-08-01 17:55:54 +0530

    Solved using Counting

--- 3rd_Streak/Day129.py
+++ 3rd_Streak/Day129.py
@@ -1,2 +1,10 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Number of Senior Citizens
+# Problem Statement : You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
+# The first ten characters consist of the phone number of passengers.
+# The next character denotes the gender of the person.
+# The following two characters are used to indicate the age of the person.
+# The last two characters determine the seat allotted to that person.
+# Return the number of passengers who are strictly more than 60 years old.
+class Solution:
+    def countSeniors(self, details: list[str]) -> int:
+        return sum([int(x[11:13]) > 60 for x in details])

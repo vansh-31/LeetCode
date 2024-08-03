43738febdca493cbb34e53a2824d467cbcdf7589
Vansh Gawra <vanshgawra008@gmail.com>
2024-08-03 18:19:20 +0530

    Solved using Hash Table

--- 3rd_Streak/Day131.py
+++ 3rd_Streak/Day131.py
@@ -1,2 +1,10 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Make Two Arrays Equal by Reversing Subarrays
+# Problem Statement : You are given two integer arrays of equal length target and arr. In one step, you can select any non-empty subarray of arr and reverse it. You are allowed to make any number of steps.
+# Return true if you can make arr equal to target or false otherwise.
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        return Counter(target) == Counter(arr)

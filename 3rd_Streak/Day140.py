8981051898d846d05e411e82d05de4a11bec57f0
Vansh Gawra <vanshgawra008@gmail.com>
2024-08-12 11:02:41 +0530

    Solved using min heap

--- 3rd_Streak/Day140.py
+++ 3rd_Streak/Day140.py
@@ -1,2 +1,21 @@
-# Problem : 
-# Problem Statement : 
+# Problem : Kth Largest Element in a Stream
+# Problem Statement : Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+# Implement KthLargest class:
+# KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+# int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+from typing import List
+from heapq import heappop, heappush
+
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = []
+        self.k = k
+        for num in nums:
+            self.add(num)
+
+    def add(self, val: int) -> int:
+        heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heappop(self.heap)
+        return self.heap[0]
